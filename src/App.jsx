69539791c58fd5631777ccd75881f3f9bba6a457
Vivek Singh <singhvivek357480@gmail.com>
2025-03-23 18:10:53 +0530

    Project initialize!

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div class="text-3xl font-bold underline">
+    Hey, I'm vivek & going to build my portfolio website!
+    </div>
+  )
+}
+
+export default App
